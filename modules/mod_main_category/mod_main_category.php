3212e13191355b0096dcfcc3e200c266412c2d06
rakatanui <cantripsun@gmail.com>
2017-04-05 15:03:21 +0300

    'bug-fix-main-page'

--- modules/mod_main_category/mod_main_category.php
+++ modules/mod_main_category/mod_main_category.php
@@ -11,48 +11,47 @@
 defined('_JEXEC') or die;
 
 ?>
-<div class="container strangecontainer">
-    <div class="row">
-        <div class="mod_main_category mod_main_category4 mod_main_category0">
-            <a href="<?php echo $params->get('url1'); ?>">
-                <div class="mod_main_category_img"><img src="<?php echo $params->get('img1'); ?>" alt=""></div>
-                <div class="mod_main_category_name"><span><?php echo $params->get('name1'); ?></span></div>
-            </a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="mod_main_category mod_main_category4 mod_main_category0">
-            <a href="<?php echo $params->get('url2'); ?>">
-                <div class="mod_main_category_img"><img src="<?php echo $params->get('img2'); ?>" alt=""></div>
-                <div class="mod_main_category_name"><span><?php echo $params->get('name2'); ?></span></div>
-            </a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="mod_main_category mod_main_category4 mod_main_category0">
-            <a href="<?php echo $params->get('url3'); ?>">
-                <div class="mod_main_category_img"><img src="<?php echo $params->get('img3'); ?>" alt=""></div>
-                <div class="mod_main_category_name"><span><?php echo $params->get('name3'); ?></span></div>
-            </a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="mod_main_category mod_main_category4 mod_main_category0">
-            <a href="<?php echo $params->get('url4'); ?>">
-                <div class="mod_main_category_img"><img src="<?php echo $params->get('img4'); ?>" alt=""></div>
-                <div class="mod_main_category_name"><span><?php echo $params->get('name4'); ?></span></div>
-            </a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="mod_main_category mod_main_category4 mod_main_category0">
-            <a href="<?php echo $params->get('url5'); ?>">
-                <div class="mod_main_category_img"><img src="<?php echo $params->get('img5'); ?>" alt=""></div>
-                <div class="mod_main_category_name"><span><?php echo $params->get('name5'); ?></span></div>
-            </a>
-        </div>
+<div class="row">
+    <div class="mod_main_category mod_main_category4 mod_main_category0">
+        <a href="<?php echo $params->get('url1'); ?>">
+            <div class="mod_main_category_img"><img src="<?php echo $params->get('img1'); ?>" alt=""></div>
+            <div class="mod_main_category_name"><span><?php echo $params->get('name1'); ?></span></div>
+        </a>
     </div>
 </div>
+<div class="row">
+    <div class="mod_main_category mod_main_category4 mod_main_category0">
+        <a href="<?php echo $params->get('url2'); ?>">
+            <div class="mod_main_category_img"><img src="<?php echo $params->get('img2'); ?>" alt=""></div>
+            <div class="mod_main_category_name"><span><?php echo $params->get('name2'); ?></span></div>
+        </a>
+    </div>
+</div>
+<div class="row">
+    <div class="mod_main_category mod_main_category4 mod_main_category0">
+        <a href="<?php echo $params->get('url3'); ?>">
+            <div class="mod_main_category_img"><img src="<?php echo $params->get('img3'); ?>" alt=""></div>
+            <div class="mod_main_category_name"><span><?php echo $params->get('name3'); ?></span></div>
+        </a>
+    </div>
+</div>
+<div class="row">
+    <div class="mod_main_category mod_main_category4 mod_main_category0">
+        <a href="<?php echo $params->get('url4'); ?>">
+            <div class="mod_main_category_img"><img src="<?php echo $params->get('img4'); ?>" alt=""></div>
+            <div class="mod_main_category_name"><span><?php echo $params->get('name4'); ?></span></div>
+        </a>
+    </div>
+</div>
+<div class="row">
+    <div class="mod_main_category mod_main_category4 mod_main_category0">
+        <a href="<?php echo $params->get('url5'); ?>">
+            <div class="mod_main_category_img"><img src="<?php echo $params->get('img5'); ?>" alt=""></div>
+            <div class="mod_main_category_name"><span><?php echo $params->get('name5'); ?></span></div>
+        </a>
+    </div>
+</div>
+
 
 <div class="line">...</div>
 <br/>
