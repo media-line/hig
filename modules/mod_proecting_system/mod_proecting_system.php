c7b6d527090d12a32e2d7350dda6db08c49458a4
rakatanui <cantripsun@gmail.com>
2017-04-06 19:02:24 +0300

    '06/04/2017'

---
+++ modules/mod_proecting_system/mod_proecting_system.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @package     Joomla.Tutorials
+ * @subpackage  Module
+ * @copyright   (C) 2015 gordoney
+ * @license     License GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die;
+
+?>
+<div class="clearfix proektsys">
+    <div class="mod_name2">Проектирование систем холодоснабжения</div>
+    <div class="wrap">
+        <div class="row">
+            <div class="item">
+                <img src="/images/other/proekting/logistic.png"/>
+                <p>Логистическим центрам</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/sklad.png"/>
+                <p>Холодильным складам</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="item">
+                <img src="/images/other/proekting/led.png"/>
+                <p>Ледовым аренам и каткам</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/ferma.png"/>
+                <p>Фермерским хозяйствам</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="item">
+                <img src="/images/other/proekting/prom.png"/>
+                <p>Пищевой и перерабатывающей промышленности</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/shop.png"/>
+                <p>Торговым сетям</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="mod_name2">Проектирование холодоснабжения включает</div>
+    <div class="wrapbot">
+        <div class="row left"></div>
+        <div class="row row"></div>
+    </div>
+</div>
---
+++ modules/mod_to/mod_to.php
@@ -0,0 +1,16 @@
+<?php
+ 
+/**
+ * @package     Joomla.Tutorials
+ * @subpackage  Module
+ * @copyright   (C) 2015 gordoney
+ * @license     License GNU General Public License version 2 or later; see LICENSE.txt
+ */
+   
+// No direct access to this file
+defined('_JEXEC') or die;
+
+?>
+<div class="clearfix">
+
+</div>
