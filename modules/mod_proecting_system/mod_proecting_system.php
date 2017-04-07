2c47718bb198e0761c629656ab6a2c679aa0c39c
rakatanui <cantripsun@gmail.com>
2017-04-07 16:46:44 +0300

    'Proektirovanie'

--- modules/mod_proecting_system/mod_proecting_system.php
+++ modules/mod_proecting_system/mod_proecting_system.php
@@ -48,7 +48,45 @@ defined('_JEXEC') or die;
 
     <div class="mod_name2">Проектирование холодоснабжения включает</div>
     <div class="wrapbot">
-        <div class="row left"></div>
-        <div class="row row"></div>
+        <div class="row row-left">
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/raschet.png" />
+                <p>Расчет холодопроизводительности оборудования</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/podbor.png" />
+                <p>Подбор холодильного оборудовнания</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/soglasovanie.png" />
+                <p>Согласование проектных решений с заказчиком</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/obvijazka.png" />
+                <p>Расстановка и обвязка холодильного оборудования</p>
+            </div>
+        </div>
+        <div class="row row-right">
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/shemi.png" />
+                <p>Разработка принципиальных схем холодильных установок</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/specifikacii.png" />
+                <p>Составление спецификации оборудования и материалов</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/zapiska.png" />
+                <p>Оформление пояснительной записки</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/chertez.png" />
+                <p>Оформление чертежей</p>
+            </div>
+            <div class="item">
+                <img src="/images/other/proekting/proekt-vkl/support.png" />
+                <p>Авторское сопровождение проекта</p>
+            </div>
+        </div>
     </div>
 </div>
---
+++ modules/mod_projects_done/mod_projects_done.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * @package     Joomla.Tutorials
+ * @subpackage  Module
+ * @copyright   (C) 2015 gordoney
+ * @license     License GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die;
+
+?>
+<div class="clearfix pdone">
+    <div class="mod_name2">Реализованные и выполненные объекты</div>
+    <div class="row">
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/_________________________________________.jpg">
+                </div>
+                <p class="nameprod">Бабушкина крынка</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Бобруйске</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/belkal.jpg">
+                </div>
+                <p class="nameprod">Беларуськалий</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Солигорске</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/8964e526a8ffc2e82a9e17c03e719bd8.png">
+                </div>
+                <p class="nameprod">Белорусьнефть</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Минске</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/2-925-5-11848-80-letnie-tradicii-gomelsteklo-640x640.jpg">
+                </div>
+                <p class="nameprod">Гомельстекло</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Гомеле</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/grodno_logo.png">
+                </div>
+                <p class="nameprod">Гродно Азот</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Гродно</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/bgu.jpg">
+                </div>
+                <p class="nameprod">БГУ</p>
+            </div>
+            <p class="descprod">Склад готовой продукции для столовой в г. Минске</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/bellatmeat.png">
+                </div>
+                <p class="nameprod">Белатмит</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Минске</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/logo_belsyr.jpg">
+                </div>
+                <p class="nameprod">БелСыр</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Гомеле</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/belalko.gif">
+                </div>
+                <p class="nameprod">БЕЛАЛКО</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Бресте</p>
+        </div>
+        <div class="item">
+            <div class="headprod">
+                <div>
+                    <img src="/components/com_jshopping/files/img_manufs/onega_big.jpg">
+                </div>
+                <p class="nameprod">Онега</p>
+            </div>
+            <p class="descprod">Склад готовой продукции в г. Минске</p>
+        </div>
+    </div>
+</div>
