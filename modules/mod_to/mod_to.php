fe1a8796ae043d8ae398430a90d46a246257ee83
rakatanui <cantripsun@gmail.com>
2017-04-14 16:47:28 +0300

    'ServisIMontaz'

--- modules/mod_to/mod_to.php
+++ modules/mod_to/mod_to.php
@@ -1,16 +1,89 @@
 <?php
- 
+
 /**
  * @package     Joomla.Tutorials
  * @subpackage  Module
  * @copyright   (C) 2015 gordoney
  * @license     License GNU General Public License version 2 or later; see LICENSE.txt
  */
-   
+
 // No direct access to this file
 defined('_JEXEC') or die;
 
 ?>
-<div class="clearfix">
-
+<div class="clearfix to">
+    <div class="row">
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/holodagreg.jpg" class="image" alt="Холодильные агрегаты">
+                <p class="name">Холодильные агрегаты</p>
+                <p class="ltlname"></p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/saduh.jpg" class="image" alt="Системы автоматики для установок холодоснабжения">
+                <p class="name">Системы автоматики</p>
+                <p class="ltlname">для установок холодоснабжения</p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/cameri.jpg" class="image" alt="Холодильные и морозильные камеры">
+                <p class="name">Холодильные</p>
+                <p class="ltlname">и морозильные камеры</p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/tho.jpg" class="image" alt="Торговое холодильное оборудование">
+                <p class="name">Торговое холодильное</p>
+                <p class="ltlname">оборудование</p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/pho.jpg" class="image" alt="Промышленное холодильное оборудование">
+                <p class="name">Промышленное</p>
+                <p class="ltlname">холодильное оборудование</p>
+            </div>
+        </a>
+    </div>
+    <div class="row">
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/centrali.jpg" class="image" alt="Системы встроенного и выносного холода">
+                <p class="name">Системы встроенного</p>
+                <p class="ltlname">и выносного холода</p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/centrali2.jpg" class="image" alt="Централи на основе компрессоров различного производства">
+                <p class="name">Централи на основе</p>
+                <p class="ltlname">компрессоров различного производства</p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/condicioneri.jpg" class="image" alt="Кондиционеры">
+                <p class="name">Кондиционеры</p>
+                <p class="ltlname"></p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/sck.jpg" class="image" alt="Системы контроля климата">
+                <p class="name">Системы контроля</p>
+                <p class="ltlname">климата</p>
+            </div>
+        </a>
+        <a href="/servis">
+            <div class="item">
+                <img src="/modules/mod_to/images/schf.jpg" class="image" alt="Системы чиллер-фанкоил">
+                <p class="name">Системы</p>
+                <p class="ltlname">чиллер-фанкоил</p>
+            </div>
+        </a>
+    </div>
 </div>
