8cb6ac2f5c7b9679ccd9a041bc6a6fdde57131dd
rakatanui <cantripsun@gmail.com>
2017-04-17 16:52:15 +0300

    ''

--- modules/mod_to2/mod_to2.php
+++ modules/mod_to2/mod_to2.php
@@ -15,28 +15,28 @@ defined('_JEXEC') or die;
     <div class="row">
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/pho.jpg" class="image" alt="Промышленное холодильное оборудование">
+                <img src="/modules/mod_to2/images/pho.jpg" class="image" alt="Промышленное холодильное оборудование">
                 <p class="name">Промышленное</p>
                 <p class="ltlname">холодильное оборудование</p>
             </div>
         </a>
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/himk.jpg" class="image" alt="Холодильные и морозильные камеры">
+                <img src="/modules/mod_to2/images/himk.jpg" class="image" alt="Холодильные и морозильные камеры">
                 <p class="name">Холодильные</p>
                 <p class="ltlname">и морозильные камеры</p>
             </div>
         </a>
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/chilleri.jpg" class="image" alt="Чиллеры">
+                <img src="/modules/mod_to2/images/chilleri.jpg" class="image" alt="Чиллеры">
                 <p class="name">Чиллеры</p>
                 <p class="ltlname"></p>
             </div>
         </a>
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/to.jpg" class="image" alt="Теплообменное оборудование">
+                <img src="/modules/mod_to2/images/to.jpg" class="image" alt="Теплообменное оборудование">
                 <p class="name">Теплообменное</p>
                 <p class="ltlname">оборудование</p>
             </div>
@@ -45,28 +45,28 @@ defined('_JEXEC') or die;
     <div class="row">
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/kbn.jpg" class="image" alt="Кондиционеры бытового назначения">
+                <img src="/modules/mod_to2/images/kbn.jpg" class="image" alt="Кондиционеры бытового назначения">
                 <p class="name">Кондиционеры</p>
                 <p class="ltlname">бытового назначения</p>
             </div>
         </a>
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/torn.jpg" class="image" alt="Технологическое оборудование различного назначения">
+                <img src="/modules/mod_to2/images/torn.jpg" class="image" alt="Технологическое оборудование различного назначения">
                 <p class="name">Технологическое</p>
                 <p class="ltlname">оборудование различного назначения</p>
             </div>
         </a>
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/sv.jpg" class="image" alt="Системы вентиляции">
+                <img src="/modules/mod_to2/images/sv.jpg" class="image" alt="Системы вентиляции">
                 <p class="name">Системы вентиляции</p>
                 <p class="ltlname"></p>
             </div>
         </a>
         <a href="/servis">
             <div class="item">
-                <img src="/modules/mod_to/images/saido.jpg" class="image" alt="Системы автоматизации и диспетчеризации оборудования">
+                <img src="/modules/mod_to2/images/saido.jpg" class="image" alt="Системы автоматизации и диспетчеризации оборудования">
                 <p class="name">Системы автоматизации</p>
                 <p class="ltlname">и диспетчеризации оборудования</p>
             </div>
